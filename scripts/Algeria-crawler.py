f7e949ac4b933fec15024a2f7afe40bf8d805cd9
Anusha <anusha25cse@gmail.com>
2021-03-09 14:09:18 -0500

    Fixed Algeria crawler

--- scripts/Algeria-crawler.py
+++ scripts/Algeria-crawler.py
@@ -11,26 +11,30 @@ import requests
 import os
 from datetime import datetime
 begin_time = datetime.now()
-url = "https://dz-covid19.com/ws/?EIO=3&transport=polling"
-sid = requests.get(url, headers={'Connection': 'close'}).text[12:32]
+#url = "https://dz-covid19.com/ws/?EIO=3&transport=polling"
+url="https://api.corona-dz.live/province/latest"
+response = requests.get(url, headers={'Connection': 'close'})
+data = json.loads(response.text)
 
-url = url + "&sid=" + sid
+#url = url + "&sid=" + sid
 
-response = requests.get(url, headers={'Connection': 'close'}).text
-data = response[response.index("wilayas") + 9:-1]
-data = json.loads(data)
+#response = requests.get(url, headers={'Connection': 'close'}).text
+#data = response[response.index("wilayas") + 9:-1]
+#data = json.loads(data)
 
 # Create and open the CSV
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
 folder_path = './data/Algeria/'+ mkfile_time + '/'
+
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 file = open(folder_path+'table.csv', 'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(file)
 
-headers = ["name", "confirmed", "deaths", "recovered", "active"]
+#headers = ["name", "confirmed", "deaths", "recovered", "active"]
+headers = ["name", "confirmed", "deaths", "recovered"]
 writer.writerow(headers)
 for d in data:
-	row = [d[h] for h in headers]
+	row = d["name"],d["data"][0]["confirmed"],d["data"][0]["deaths"],d["data"][0]["recovered"]
 	writer.writerow(row)
 print(datetime.now() - begin_time)
