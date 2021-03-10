f8d9544dcca99f775f7bc80bf201e45f9eda5219
Anusha <anusha25cse@gmail.com>
2021-03-09 19:37:45 -0500

    Fixed Greece crawler

--- scripts/Greece-crawler.py
+++ scripts/Greece-crawler.py
@@ -12,12 +12,24 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-url = 'https://github.com/iMEdD-Lab/open-data/blob/master/COVID-19/regions_greece.csv'
+url = 'https://github.com/iMEdD-Lab/open-data/blob/master/COVID-19/greece_latest.csv'
 
+# empty list
+data = []
 # Get the data
 response = requests.get(url, headers={'Connection': 'close'})
+
 soup = BeautifulSoup(response.content, 'lxml')
-tag = soup.findAll('tbody')[0].contents[1::2]
+HTML_data_header = soup.find_all("table")[0].find_all("th")
+HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+
+header=[]
+for each_header in HTML_data_header:
+    header.append(each_header.get_text())
+
+
+# Write each line to the csv
+headers = ["Region", "LocalName", "Cases", "Deaths"]
 
 # Create and open the CSV
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
@@ -26,12 +38,20 @@ if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 file = open(folder_path+'table.csv', 'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(file)
+writer.writerow([header[0],header[1],header[7],header[8]])
 
-# Write each line to the csv
-headers = ["Region", "LocalName", "Cases", "Deaths"]
-writer.writerow(headers)
-for t in tag:
-	t = t.contents[1::2]
-	row = [t[2].contents[0], t[1].contents[0], t[4].contents[0], t[5].contents[0]]
-	writer.writerow(row)
-	##Test
+
+# for getting the data
+for element in HTML_data:
+    sub_data = []
+    for sub_element in element:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
+
+for d in data:
+    row = [d[1],d[2],d[8],d[9]]
+    writer.writerow(row)
