c0f08cbd0de8df583fe7d1f14855921c416c3a32
Anusha <anusha25cse@gmail.com>
2021-03-10 09:28:04 -0500

    Update Croatia Crawler

--- scripts/Croatia-crawler.py
+++ scripts/Croatia-crawler.py
@@ -10,12 +10,17 @@ import requests
 import json
 import os
 from datetime import datetime
+from bs4 import BeautifulSoup
 
-url = "https://www.koronavirus.hr/json/?action=po_danima_zupanijama_zadnji"
+# empty list
+data = []
 
-# Get the data
+url = "https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Croatia"
 response = requests.get(url, headers={'Connection': 'close'})
-data = json.loads(response.text)[0]["PodaciDetaljno"]
+soup = BeautifulSoup(response.content, 'lxml')
+html_content = soup.find("table",{"class":"wikitable"}).find_all("tr")[1:22]
+
+headers = ["Region", "Cases", "Deaths", "Active"]
 
 # Create and open the CSV
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
@@ -24,10 +29,24 @@ if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 file = open(folder_path+'table.csv', 'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(file)
-
-# Write each line to the CSV
-headers = ["Region", "Cases", "Deaths", "Active"]
 writer.writerow(headers)
+
+for each_row in html_content:
+    sub_data = []
+    links = each_row.find('a', href=True)
+    try:
+        title = links.attrs['title']
+        sub_data.append(title)
+    except AttributeError:
+        pass
+
+    for sub_element in each_row:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
 for d in data:
-	row = [d["Zupanija"], d["broj_zarazenih"], d["broj_umrlih"], d["broj_aktivni"]]
-	writer.writerow(row)
+    row = [d[0].strip('\n'),d[2].strip('\n'),d[4].strip('\n'),d[6].strip('\n')]
+    writer.writerow(row)
