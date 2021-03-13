02e53a8a6fa6e0d448386517c1c63ee5f54eb7d1
AnushaSrirenganathan <anusha25cse@gmail.com>
2021-03-13 11:15:00 -0600

    Update SierraLeone-crawler.py

--- scripts/SierraLeone-crawler.py
+++ scripts/SierraLeone-crawler.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 import os
 from datetime import datetime
 
-url = 'https://covid19.mic.gov.sl/'
-url = 'https://covid19.mic.gov.sl/static/media/districts.beade2ed.csv'
+url = 'http://dhse.gov.sl/'
+
 
 # Make the directory
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
@@ -21,33 +21,45 @@ folder_path = './data/SierraLeone/'+ mkfile_time + '/'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
+eastern = ["Kailahun", "Kono", "Kenema"]
+northern = ["Bombali", "Falaba", "Kambia", "Koinadugu", "Portloko", "Tonkolili"] 
+southern = ["Bo", "Bonthe", "Moyamba", "Pujehun"]
+western = ["Western Rural", "Western Urban"]
+
 # Download the data
 req = requests.get(url).content.decode('utf-8')
 
 # Create the dictrict csv
 file = open(folder_path+'districts.csv', 'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(file)
-file.write(req)
-file.close()
-
-# Create a names dictionary
-req = req.splitlines()
-names = ast.literal_eval("['" + req[0].replace(",","','") + "']")
-n = {k: v for v, k in enumerate(names)}
-
-eastern = ["kailahun", "kono", "kenema"]
-northern = ["bombali", "falaba", "kambia", "koinadugu", "portloko", "tonkolili"] 
-southern = ["bo", "bonthe", "moyamba", "pujehun"]
-western = ["western rural", "western urban"]
+labels = ["District", "Confirmed Cases"]
+writer.writerow(labels)
 
 # Create the region csv
-file = open(folder_path+'regions.csv', 'w', newline='', encoding='utf-8-sig')
-writer = csv.writer(file)
-labels = ["Date", "Eastern", "Northern", "Southern", "Western"]
-writer.writerow(labels)
-req = ["['"+r.replace(",","',",1)+"]" for r in req[1:]]
-req = [ast.literal_eval(r) for r in req]
+rfile = open(folder_path+'regions.csv', 'w', newline='', encoding='utf-8-sig')
+rwriter = csv.writer(rfile)
+rlabels = ["Eastern", "Northern", "Southern", "Western"]
+rwriter.writerow(rlabels)
 
-for r in req:
-	row = [r[0], sum([r[n[i]] for i in eastern]), sum([r[n[i]] for i in northern]), sum([r[n[i]] for i in southern]), sum([r[n[i]] for i in western])]
-	writer.writerow(row)
+# Download the data
+#requests.packages.urllib3.disable_warnings()
+ecount = wcount = ncount = scount = 0
+response = requests.get(url, headers={'Connection': 'close'})
+soup = BeautifulSoup(response.content, 'lxml')
+html_content = soup.findAll("div",{"class":"elementor-text-editor elementor-clearfix"})[19:55]
+
+for i in range(0,len(html_content)-1,2):
+	writer.writerow([html_content[i].text, html_content[i+1].text])
+	if html_content[i].text in eastern:
+		ecount += int(html_content[i+1].text)
+	if html_content[i].text in western:
+		wcount += int(html_content[i+1].text)
+	if html_content[i].text in northern:
+		ncount += int(html_content[i+1].text)
+	if html_content[i].text in southern:
+		scount += int(html_content[i+1].text)
+
+rtotal = [ecount,wcount,ncount,scount]
+rwriter.writerow(rtotal)
+file.close()
+rfile.close()
