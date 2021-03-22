1d520b31e1d2c7bdf87f9df975fe0e89253b993a
AnushaSrirenganathan <anusha25cse@gmail.com>
2021-03-22 01:09:34 -0500

    Update Bolivia-crawler.py

--- scripts/Bolivia-crawler.py
+++ scripts/Bolivia-crawler.py
@@ -11,37 +11,25 @@ from bs4 import BeautifulSoup
 import os
 import time
 from datetime import datetime
-import pandas as pd 
+import pandas as pd
 begin_time = datetime.now()
 url = 'https://www.boliviasegura.gob.bo/'
 
 
 response = requests.get(url, headers={'Connection': 'close'})
 soup = BeautifulSoup(response.content, 'lxml')
-items = soup.find_all('img')
-
+items = soup.find_all("div", {"class": "carousel-item"})
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
 
-folder_path = './data/Bolivia/'+ mkfile_time + '/'
-if os.path.exists(folder_path) == False:  # 判断文件夹是否已经存在
-    os.makedirs(folder_path)
+folder_path = './data/Bolivia/' + mkfile_time + '/'
 
-
-try:
-	for index, item in enumerate(items):
-		if (item.get('src').find('https://') > -1):
-			# get函数获取图片链接地址，requests发送访问请求
-			html = requests.get(item.get('src'))
-			img_name = folder_path + str(index + 1) + '.png'
-			print(img_name)
-			with open(img_name, 'wb') as file:
-			    file.write(html.content)
-			    file.flush()
-			file.close()
-			print('第%d张图片下载完成' % (index+1))
-			time.sleep(1)  # 自定义延时
-
-
-except IOError:
-	print("error")
-print(datetime.now() - begin_time)
+for index, item in enumerate(items):
+    image = item.img['src']
+    html = requests.get(image)
+    img_name = folder_path + str(index + 1) + '.png'
+    with open(img_name, 'wb') as file:
+        file.write(html.content)
+        file.flush()
+    file.close()
+    print('第%d张图片下载完成' % (index+1))
+    time.sleep(1)  # 自定义延时
